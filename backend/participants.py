6c2d92d92a9ccda357bc77fb5de55f10626e4f12
111161226 <fanrin9@gmail.com>
2024-05-07 15:49:35 +0900

    add participant object

---
+++ backend/participants.py
@@ -0,0 +1,15 @@
+import participant
+
+class ParticipantList:
+    def __init__(self, num):
+        self._participants=[]
+        for _ in range(num):
+            id=len(self._participants)+1
+            self.add_participant(id)
+    
+    def get_participants(self):
+        return self._participants
+    
+    def add_participant(self, id):
+        participant=Participant(id, id)
+        self._participants.append(participant)
