ef8b39f8fbf36602786fe73b5a1dff8ed08bed7e
inatoihs <sabanna.taihei@gmail.com>
2024-05-07 14:11:45 +0900

    send message endpoint

--- server.py
+++ server.py
@@ -9,11 +9,11 @@ def hello():
     return "Hello World!"
 
 
-# 参加者の人数をから受け取る
-@app.route("/register", methods=["POST"])
-def register():
-    n = request.get_json()["n"]
-    return f"You are going to talk with {n} people."
+# ユーザーIDとメッセージの内容を受け取る
+@app.route("/send", methods=["POST"])
+def send():
+    data = request.get_json()
+    return f"User{data['id']} said '{data['message']}'."
 
 
 if __name__ == "__main__":
