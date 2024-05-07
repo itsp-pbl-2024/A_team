eb8c406f63abc25fb47ce11faa224bef045865c5
inatoihs <sabanna.taihei@gmail.com>
2024-05-07 14:10:17 +0900

    ユーザーの人数を受け取る

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask import request
 
 app = Flask(__name__)
 
@@ -6,6 +7,13 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     return "Hello World!"
+
+
+# 参加者の人数をから受け取る
+@app.route("/register", methods=["POST"])
+def register():
+    n = request.get_json()["n"]
+    return f"You are going to talk with {n} people."
 
 
 if __name__ == "__main__":
