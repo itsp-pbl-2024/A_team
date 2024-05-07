2e07336703f6532fe901f5312366da5cf6af380b
inatoihs <sabanna.taihei@gmail.com>
2024-05-07 14:22:49 +0900

    feature01and02

--- backend/server.py
+++ backend/server.py
@@ -9,6 +9,13 @@ def hello():
     return "Hello World!"
 
 
+# 参加者の人数をから受け取る
+@app.route("/register", methods=["POST"])
+def register():
+    n = request.get_json()["n"]
+    return f"You are going to talk with {n} people."
+
+
 # ユーザーIDとメッセージの内容を受け取る
 @app.route("/send", methods=["POST"])
 def send():
