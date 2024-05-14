aa2c4d160fbce64fb9ba3a956bd727b485cd99bc
inatoihs <sabanna.taihei@gmail.com>
2024-05-14 19:01:15 +0900

    各テストを独立させました

--- test_module.py
+++ test_module.py
@@ -1,48 +1,16 @@
 import pytest
 
-"""
-# 参加者の人数をから受け取る
-@app.route("/register", methods=["POST"])
-def register():
-    data = request.get_json()
-    if "n" not in data:
-        return "Invalid request."
-    n = data["n"]
-    if n <= 0:
-        return "Invalid number of people."
-    n = request.get_json()["n"]
-    room_id = all_room.create_room(n)
-    return f"You are going to talk with {n} people."
-
-
-# ユーザーIDとメッセージの内容を受け取る
-@app.route("/send", methods=["POST"])
-def send():
-    data = request.get_json()
-    if "id" not in data or "message" not in data:
-        return "Invalid request."
-    room = all_room.get_room(data["room_id"])
-    message = Message(data)
-    room.add_message(message)
-    return f"User{data['id']} said '{data['message']}'."
-
-
-@app.route("/check", methods=["GET"])
-def check():
-    # id=f()
-    return "id" + " is quiet"
-"""
 # テスト対象APIコードのappをインポート
-from backend.server import app
-
-# テスト用コンフィグをtrueに設定
-app.config["TESTING"] = True
-# テスト対象API呼び出し用テストクライアント生成
-client = app.test_client()
+from backend.server import create_app
 
 
 # hello
 def test_flask_N001():
+    app = create_app()
+    # テスト用コンフィグをtrueに設定
+    app.config["TESTING"] = True
+    # テスト対象API呼び出し用テストクライアント生成
+    client = app.test_client()
     # テスト対象API実行
     result = client.get("/")
     assert result.status_code == 200
@@ -51,6 +19,11 @@ def test_flask_N001():
 
 # register successful case
 def test_flask_N002():
+    app = create_app()
+    # テスト用コンフィグをtrueに設定
+    app.config["TESTING"] = True
+    # テスト対象API呼び出し用テストクライアント生成
+    client = app.test_client()
     for i in range(1, 3):
         result = client.post("/register", json={"n": i})
         assert result.status_code == 200
@@ -59,6 +32,11 @@ def test_flask_N002():
 
 # register failed case (Invalid request)
 def test_flask_N003():
+    app = create_app()
+    # テスト用コンフィグをtrueに設定
+    app.config["TESTING"] = True
+    # テスト対象API呼び出し用テストクライアント生成
+    client = app.test_client()
     result = client.post("/register", json={"m": 10})
     assert result.status_code == 200
     assert b"Invalid request." == result.data
@@ -66,6 +44,11 @@ def test_flask_N003():
 
 # register failed case (Invalid number of people)
 def test_flask_N004():
+    app = create_app()
+    # テスト用コンフィグをtrueに設定
+    app.config["TESTING"] = True
+    # テスト対象API呼び出し用テストクライアント生成
+    client = app.test_client()
     result = client.post("/register", json={"n": 0})
     assert result.status_code == 200
     assert b"Invalid number of people." == result.data
@@ -73,6 +56,12 @@ def test_flask_N004():
 
 # send successful case
 def test_flask_N005():
+    app = create_app()
+    # テスト用コンフィグをtrueに設定
+    app.config["TESTING"] = True
+    # テスト対象API呼び出し用テストクライアント生成
+    client = app.test_client()
+    client.post("/register", json={"n": 11})
     result = client.post("/send", json={"id": 10, "message": "Hello"})
     assert result.status_code == 200
     assert b"User10 said 'Hello'." == result.data
@@ -80,6 +69,11 @@ def test_flask_N005():
 
 # registerで人数登録して、sendで複数人のメッセージを送信する。最後にcheckでメッセージを確認する
 def test_flask_S001():
+    app = create_app()
+    # テスト用コンフィグをtrueに設定
+    app.config["TESTING"] = True
+    # テスト対象API呼び出し用テストクライアント生成
+    client = app.test_client()
     # register
     result = client.post("/register", json={"n": 3})
     assert result.status_code == 200
@@ -103,26 +97,37 @@ def test_flask_S001():
     # check
     result = client.get("/check")
     assert result.status_code == 200
-    assert b"id is quiet" == result.data  # TODO: id 2 is quiet
+    assert b"2 is quiet" == result.data
 
 
 # id 1がquietの場合
 def test_flask_S002():
+    app = create_app()
+    # テスト用コンフィグをtrueに設定
+    app.config["TESTING"] = True
+    # テスト対象API呼び出し用テストクライアント生成
+    client = app.test_client()
     # register
     client.post("/register", json={"n": 3})
 
     # send
-    client.post("/send", json={"id": 2, "room_id": 1, "message": "Hi"})
+    client.post("/send", json={"id": 1, "room_id": 1, "message": "Hi"})
     client.post("/send", json={"id": 3, "room_id": 1, "message": "Good morning"})
     client.post("/send", json={"id": 2, "room_id": 1, "message": "How are you?"})
 
     # check
     result = client.get("/check")
     assert result.status_code == 200
-    assert b"id is quiet" == result.data  # TODO: id 1 is quiet
+    assert b"1 is quiet" == result.data
 
 
+# id 4が話しておらず、quietの場合
 def test_flask_S003():
+    app = create_app()
+    # テスト用コンフィグをtrueに設定
+    app.config["TESTING"] = True
+    # テスト対象API呼び出し用テストクライアント生成
+    client = app.test_client()
     # register
     client.post("/register", json={"n": 5})
 
@@ -134,4 +139,4 @@ def test_flask_S003():
     # check
     result = client.get("/check")
     assert result.status_code == 200
-    assert b"id is quiet" == result.data  # TODO: id 4 is quiet
+    assert b"4 is quiet" == result.data
