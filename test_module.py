877af81bf5c9d2bfaf4571a6abd49f3911ab7fd0
inatoihs <sabanna.taihei@gmail.com>
2024-05-14 13:58:56 +0900

    不要なコメントの削除

--- test_module.py
+++ test_module.py
@@ -1,43 +1,5 @@
 import pytest
 
-"""
-from flask import Flask
-from flask import request
-
-app = Flask(__name__)
-
-
-@app.route("/")
-def hello():
-    return "Hello World!"
-
-
-# 参加者の人数をから受け取る
-@app.route("/register", methods=["POST"])
-def register():
-    n = request.get_json()["n"]
-    return f"You are going to talk with {n} people."
-
-
-# ユーザーIDとメッセージの内容を受け取る
-@app.route("/send", methods=["POST"])
-def send():
-    data = request.get_json()
-    return f"User{data['id']} said '{data['message']}'."
-
-
-@app.route("/check", methods=["GET"])
-def check():
-    # id=f()
-    return "id" + "is quiet"
-
-
-if __name__ == "__main__":
-    app.run()
-
-これをテストするためのコードを書いていきます。
-"""
-
 # テスト対象APIコードのappをインポート
 from backend.server import app
 
