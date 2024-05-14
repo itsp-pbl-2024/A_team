0cfefeac816c72593297e925151736444ca66f0d
inatoihs <sabanna.taihei@gmail.com>
2024-05-14 13:57:21 +0900

    register, sendエンドポイントのテストを追加

--- test_module.py
+++ test_module.py
@@ -1,5 +1,43 @@
 import pytest
 
+"""
+from flask import Flask
+from flask import request
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello():
+    return "Hello World!"
+
+
+# 参加者の人数をから受け取る
+@app.route("/register", methods=["POST"])
+def register():
+    n = request.get_json()["n"]
+    return f"You are going to talk with {n} people."
+
+
+# ユーザーIDとメッセージの内容を受け取る
+@app.route("/send", methods=["POST"])
+def send():
+    data = request.get_json()
+    return f"User{data['id']} said '{data['message']}'."
+
+
+@app.route("/check", methods=["GET"])
+def check():
+    # id=f()
+    return "id" + "is quiet"
+
+
+if __name__ == "__main__":
+    app.run()
+
+これをテストするためのコードを書いていきます。
+"""
+
 # テスト対象APIコードのappをインポート
 from backend.server import app
 
@@ -14,3 +52,23 @@ def test_flask_N001():
     result = client.get("/")
     assert result.status_code == 200
     assert b"Hello World!" == result.data
+
+
+def test_flask_N002():
+    app.config["TESTING"] = True
+    client = app.test_client()
+
+    # テスト対象API実行
+    result = client.post("/register", json={"n": 3})
+    assert result.status_code == 200
+    assert b"You are going to talk with 3 people." == result.data
+
+
+def test_flask_N003():
+    app.config["TESTING"] = True
+    client = app.test_client()
+
+    # テスト対象API実行
+    result = client.post("/send", json={"id": 1, "message": "Hello"})
+    assert result.status_code == 200
+    assert b"User1 said 'Hello'." == result.data
