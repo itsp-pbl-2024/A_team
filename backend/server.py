ad7b40c8458848a2467e4638d28e66f682d4ec2f
sinc865 <ota.s.ad@m.titech.ac.jp>
2024-06-09 18:35:42 +0900

    エンドポイントの処理を別に分けた

--- backend/server.py
+++ backend/server.py
@@ -1,47 +1,26 @@
 from flask import Flask
-from flask import request
 from rooms import Rooms
-from message import Message
+from routes.register import register_blueprint
+from routes.send import send_blueprint
+from routes.check import check_blueprint
 
 
 def create_app():
     app = Flask(__name__)
-    all_room = Rooms()
-    ROOM_ID_DEMO = 1
+    app.config["ROOMS"] = Rooms()
+    app.config["ROOM_ID_DEMO"] = 1
+
+    # 参加者の人数をJSONファイルから受け取りROOMを作成
+    app.register_blueprint(register_blueprint)
+
+    # ユーザーIDとメッセージの内容を受け取る
+    app.register_blueprint(send_blueprint)
+
+    app.register_blueprint(check_blueprint)
 
     @app.route("/")
     def hello():
         return "Hello World!"
-
-    # 参加者の人数をから受け取る
-    @app.route("/register", methods=["POST"])
-    def register():
-        data = request.get_json()
-        if "n" not in data:
-            return "Invalid request."
-        n = data["n"]
-        if n <= 0:
-            return "Invalid number of people."
-        n = request.get_json()["n"]
-        all_room.create_room(n)
-        return f"You are going to talk with {n} people."
-
-    # ユーザーIDとメッセージの内容を受け取る
-    @app.route("/send", methods=["POST"])
-    def send():
-        data = request.get_json()
-        if "id" not in data or "message" not in data:
-            return "Invalid request."
-        room = all_room.get_room(ROOM_ID_DEMO)
-        message = Message(data)
-        room.add_message(message)
-        return f"User{data['id']} said '{data['message']}'."
-
-    @app.route("/check", methods=["GET"])
-    def check():
-        room = all_room.get_room(ROOM_ID_DEMO)
-        quiet_id = room.search_alone().get_user_name()
-        return f"{quiet_id}" + " is quiet"
 
     return app
 
