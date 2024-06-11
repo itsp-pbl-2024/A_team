0865929691e087458137bbe1ecb6fcece8ed936d
111161226 <fanrin9@gmail.com>
2024-06-11 16:44:49 +0900

    delete participant

--- backend/routes/check.py
+++ backend/routes/check.py
@@ -6,5 +6,5 @@ check_blueprint = Blueprint("check", __name__)
 @check_blueprint.route("/check", methods=["GET"])
 def check():
     room = current_app.config["ROOMS"].get_room(current_app.config["ROOM_ID_DEMO"])
-    quiet_id = room.search_alone().get_user_name()
+    quiet_id = room.search_alone()
     return f"{quiet_id} is quiet"
