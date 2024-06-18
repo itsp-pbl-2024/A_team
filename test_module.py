b9dbfd1e2f4890e39d3d3cecfc88424dffe8e35b
YasuharaKota <yasuhara.k.aa@m.titech.ac.jp>
2024-06-18 14:26:04 +0900

    Update test_module.py

--- test_module.py
+++ test_module.py
@@ -37,7 +37,7 @@ def test_flask_N003():
     app.config["TESTING"] = True
     # テスト対象API呼び出し用テストクライアント生成
     client = app.test_client()
-    result = client.post("/register", json={"m": 10})
+    result = client.post("/register", json={"n": 10})
     assert result.status_code == 200
     assert b"Invalid request." == result.data
 
@@ -62,9 +62,9 @@ def test_flask_N005():
     # テスト対象API呼び出し用テストクライアント生成
     client = app.test_client()
     client.post("/register", json={"n": 11})
-    result = client.post("/send", json={"id": 10, "message": "Hello"})
+    result = client.post("/send", json={"id": 10, "durations": ["6.4"]})
     assert result.status_code == 200
-    assert b"User10 said 'Hello'." == result.data
+    assert b'User_id: 10, durations: ["6.4"]' == result.data
 
 
 # registerで人数登録して、sendで複数人のメッセージを送信する。最後にcheckでメッセージを確認する
@@ -80,27 +80,25 @@ def test_flask_S001():
     assert b"You are going to talk with 3 people." == result.data
 
     # send
-    result = client.post("/send", json={"id": 1, "room_id": 1, "message": "Hello"})
+    result = client.post("/send", json={"id": 0, "room_id": 1, "durations": ["3.5"]})
     assert result.status_code == 200
-    assert b"User1 said 'Hello'." == result.data
+    assert b'User_id: 0, durations: ["3.5"]' == result.data
 
-    result = client.post("/send", json={"id": 2, "room_id": 1, "message": "Hi"})
+    result = client.post("/send", json={"id": 1, "room_id": 1, "durations": ["1.1"]})
     assert result.status_code == 200
-    assert b"User2 said 'Hi'." == result.data
+    assert b'User_id: 1, durations: ["1.1"]' == result.data
 
-    result = client.post(
-        "/send", json={"id": 3, "room_id": 1, "message": "Good morning"}
-    )
+    result = client.post("/send", json={"id": 2, "room_id": 1, "durations": ["6.4"]})
     assert result.status_code == 200
-    assert b"User3 said 'Good morning'." == result.data
+    assert b'User_id: 2, durations: ["6.4"]' == result.data
 
     # check
     result = client.get("/check")
     assert result.status_code == 200
-    assert b"2 is quiet" == result.data
+    assert b"1 is quiet" == result.data
 
 
-# id 1がquietの場合
+# id 0がquietの場合
 def test_flask_S002():
     app = create_app()
     # テスト用コンフィグをtrueに設定
@@ -111,17 +109,17 @@ def test_flask_S002():
     client.post("/register", json={"n": 3})
 
     # send
-    client.post("/send", json={"id": 1, "room_id": 1, "message": "Hi"})
-    client.post("/send", json={"id": 3, "room_id": 1, "message": "Good morning"})
-    client.post("/send", json={"id": 2, "room_id": 1, "message": "How are you?"})
+    client.post("/send", json={"id": 1, "room_id": 1, "durations": ["1.6"]})
+    client.post("/send", json={"id": 3, "room_id": 1, "durations": ["3.2"]})
+    client.post("/send", json={"id": 2, "room_id": 1, "durations": ["4.0"]})
 
     # check
     result = client.get("/check")
     assert result.status_code == 200
-    assert b"1 is quiet" == result.data
+    assert b"0 is quiet" == result.data
 
 
-# id 4が話しておらず、quietの場合
+# id 3が話しておらず、quietの場合
 def test_flask_S003():
     app = create_app()
     # テスト用コンフィグをtrueに設定
@@ -132,11 +130,139 @@ def test_flask_S003():
     client.post("/register", json={"n": 5})
 
     # send
-    client.post("/send", json={"id": 1, "room_id": 1, "message": "Hello"})
-    client.post("/send", json={"id": 2, "room_id": 1, "message": "Hi"})
-    client.post("/send", json={"id": 3, "room_id": 1, "message": "Good morning"})
+    client.post("/send", json={"id": 0, "room_id": 1, "durations": ["1.4"]})
+    client.post("/send", json={"id": 1, "room_id": 1, "durations": ["2.4"]})
+    client.post("/send", json={"id": 2, "room_id": 1, "durations": ["3.4"]})
+    client.post("/send", json={"id": 4, "room_id": 1, "durations": ["0.4"]})
+
+    # check
+    result = client.get("/check")
+    assert result.status_code == 200
+    assert b"3 is quiet" == result.data
+
+
+# 同率の場合はidが一番小さいuserが呼ばれる
+def test_flask_S004():
+    app = create_app()
+    # テスト用コンフィグをtrueに設定
+    app.config["TESTING"] = True
+    # テスト対象API呼び出し用テストクライアント生成
+    client = app.test_client()
+    # register
+    client.post("/register", json={"n": 5})
+
+    # send
+    client.post("/send", json={"id": 0, "room_id": 1, "durations": ["3.2"]})
+    client.post("/send", json={"id": 1, "room_id": 1, "durations": ["1.2"]})
+    client.post("/send", json={"id": 2, "room_id": 1, "durations": ["2.2"]})
+    client.post("/send", json={"id": 3, "room_id": 1, "durations": ["1.2"]})
+    client.post("/send", json={"id": 4, "room_id": 1, "durations": ["1.2"]})
+
+    # check
+    result = client.get("/check")
+    assert result.status_code == 200
+    assert b"1 is quiet" == result.data
+
+
+# 2つ要素
+def test_flask_S005():
+    app = create_app()
+    # テスト用コンフィグをtrueに設定
+    app.config["TESTING"] = True
+    # テスト対象API呼び出し用テストクライアント生成
+    client = app.test_client()
+    # register
+    client.post("/register", json={"n": 5})
+
+    # send
+    client.post("/send", json={"id": 0, "room_id": 1, "durations": ["1.9", "9.1"]})
+    client.post("/send", json={"id": 1, "room_id": 1, "durations": ["2.9", "7.1"]})
+    client.post("/send", json={"id": 2, "room_id": 1, "durations": ["3.9", "5.1"]})
+    client.post("/send", json={"id": 3, "room_id": 1, "durations": ["4.9", "3.1"]})
+    client.post("/send", json={"id": 4, "room_id": 1, "durations": ["5.9", "1.1"]})
 
     # check
     result = client.get("/check")
     assert result.status_code == 200
     assert b"4 is quiet" == result.data
+
+
+# 3つ以上
+def test_flask_S006():
+    app = create_app()
+    # テスト用コンフィグをtrueに設定
+    app.config["TESTING"] = True
+    # テスト対象API呼び出し用テストクライアント生成
+    client = app.test_client()
+    # register
+    client.post("/register", json={"n": 5})
+
+    # send
+    client.post("/send", json={"id": 0, "room_id": 1, "durations": ["1.0", "1.0", "2.2"]})
+    client.post("/send", json={"id": 1, "room_id": 1, "durations": ["3.0", "0.1", "0.1", "0.1", "0.6"]})
+    client.post("/send", json={"id": 2, "room_id": 1, "durations": ["4.0", "1.0"]})
+    client.post("/send", json={"id": 3, "room_id": 1, "durations": ["5.0"]})
+    client.post("/send", json={"id": 4, "room_id": 1, "durations": ["3.2", "2.3", "4.1", "1.5", "2.2", "0.5"]})
+
+    # check
+    result = client.get("/check")
+    assert result.status_code == 200
+    assert b"1 is quiet" == result.data
+
+
+# 1人が複数回send発行
+def test_flask_S007():
+    app = create_app()
+    # テスト用コンフィグをtrueに設定
+    app.config["TESTING"] = True
+    # テスト対象API呼び出し用テストクライアント生成
+    client = app.test_client()
+    # register
+    client.post("/register", json={"n": 5})
+
+    # send
+    client.post("/send", json={"id": 0, "room_id": 1, "durations": ["7.5"]})
+    client.post("/send", json={"id": 1, "room_id": 1, "durations": ["0.1"]})
+    client.post("/send", json={"id": 1, "room_id": 1, "durations": ["4.0"]})
+    client.post("/send", json={"id": 1, "room_id": 1, "durations": ["0.1"]})
+    client.post("/send", json={"id": 2, "room_id": 1, "durations": ["0.1", "2.2"]})
+    client.post("/send", json={"id": 3, "room_id": 1, "durations": ["0.4"]})
+    client.post("/send", json={"id": 3, "room_id": 1, "durations": ["0.5"]})
+    client.post("/send", json={"id": 3, "room_id": 1, "durations": ["0.4", "1.1"]})
+    client.post("/send", json={"id": 4, "room_id": 1, "durations": ["10.2"]})
+
+    # check
+    result = client.get("/check")
+    assert result.status_code == 200
+    assert b"3 is quiet" == result.data
+
+
+# 複数回check
+def test_flask_S008():
+    app = create_app()
+    # テスト用コンフィグをtrueに設定
+    app.config["TESTING"] = True
+    # テスト対象API呼び出し用テストクライアント生成
+    client = app.test_client()
+    # register
+    client.post("/register", json={"n": 5})
+
+    # send
+    client.post("/send", json={"id": 0, "room_id": 1, "durations": ["3.0"]})
+    client.post("/send", json={"id": 1, "room_id": 1, "durations": ["2.0"]})
+    client.post("/send", json={"id": 2, "room_id": 1, "durations": ["4.0"]})
+    client.post("/send", json={"id": 3, "room_id": 1, "durations": ["5.0"]})
+    client.post("/send", json={"id": 4, "room_id": 1, "durations": ["6.0"]})
+
+    # check
+    result = client.get("/check")
+    assert result.status_code == 200
+    assert b"1 is quiet" == result.data
+
+    # send
+    client.post("/send", json={"id": 1, "room_id": 1, "durations": ["18.0"]})
+
+    # check
+    result = client.get("/check")
+    assert result.status_code == 200
+    assert b"0 is quiet" == result.data
