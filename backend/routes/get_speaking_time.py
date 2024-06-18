19538e08ceea1a67c75ba3a7e041cd57efd9bd8e
YasuharaKota <yasuhara.k.aa@m.titech.ac.jp>
2024-06-18 16:58:45 +0900

    発話量を返すメソッドの追加

---
+++ backend/routes/get_speaking_time.py
@@ -0,0 +1,17 @@
+from flask import Blueprint, request, current_app
+from util import is_int
+
+get_speaking_time_blueprint = Blueprint("get_speaking_time", __name__)
+
+
+@get_speaking_time_blueprint.route("/get_speaking_time", methods=["GET"])
+def get_speaking_time():
+    data = request.get_json()
+    if "id" not in data:
+        return "Invalid request."
+    room = current_app.config["ROOMS"].get_room(current_app.config["ROOM_ID_DEMO"])
+    id = data["id"]
+    if not is_int(id) or id < 0 or id >= len(room.get_participants().get_participants()):
+        return "Invalid request."
+    latests = room.get_latests()
+    return f"User{id} talking time: {latests[int(id)]}"
