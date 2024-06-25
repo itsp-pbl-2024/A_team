74006a102749f5e4a40a0c6443d9ad17d04ffb90
inatoihs <sabanna.taihei@gmail.com>
2024-06-25 15:33:54 +0900

    進捗

--- interface/gui/app_layout.py
+++ interface/gui/app_layout.py
@@ -1,21 +1,30 @@
 import flet as ft
 from .chart import create_bar_chart, update_chart, reset_chart
+from speaker_diarization.diarization import MySpeakerDiarization
 
 
-def create_name_and_record_fields(num_speakers, name_fields, record_buttons, recording_states, toggle_recording):
+def create_name_and_record_fields(
+    num_speakers, name_fields, record_buttons, recording_states, toggle_recording
+):
     name_fields.clear()
     record_buttons.clear()
     recording_states.clear()
     for i in range(num_speakers):
         name_field = ft.TextField(label=f"話者{i+1}の名前")
-        record_button = ft.IconButton(icon=ft.icons.MIC, icon_color=ft.colors.RED, icon_size=40)
+        record_button = ft.IconButton(
+            icon=ft.icons.MIC_OFF, icon_color=ft.colors.RED, icon_size=40
+        )
         record_button.on_click = toggle_recording(i)
         name_fields.append(name_field)
         record_buttons.append(record_button)
         recording_states.append(False)
 
     name_and_record_fields = [
-        ft.Row([name_fields[i], record_buttons[i]], alignment=ft.MainAxisAlignment.CENTER, height=50)
+        ft.Row(
+            [name_fields[i], record_buttons[i]],
+            alignment=ft.MainAxisAlignment.CENTER,
+            height=50,
+        )
         for i in range(num_speakers)
     ]
     return name_and_record_fields
@@ -35,6 +44,7 @@ def create_centered_container(content_list):
 
 def main():
     def main_app(page: ft.Page):
+        diarization_pipeline = None
         page.title = "発言量計測アプリ"
         page.window_width = 700
         page.window_height = 500
@@ -43,7 +53,8 @@ def main():
 
         speaker_count = ft.Dropdown(
             width=100,
-            options=[ft.dropdown.Option("-")] + [ft.dropdown.Option(str(i)) for i in range(2, 6)],
+            options=[ft.dropdown.Option("-")]
+            + [ft.dropdown.Option(str(i)) for i in range(2, 6)],
             value="-",
         )
 
@@ -60,10 +71,16 @@ def main():
                 ft.Row(
                     [
                         ft.ElevatedButton(
-                            text="更新", on_click=lambda e: update_chart(chart, least_speaker_text, page)
+                            text="更新",
+                            on_click=lambda e: update_chart(
+                                chart, least_speaker_text, page
+                            ),
                         ),
                         ft.ElevatedButton(
-                            text="リセット", on_click=lambda e: reset_chart(chart, least_speaker_text, page)
+                            text="リセット",
+                            on_click=lambda e: reset_chart(
+                                chart, least_speaker_text, page
+                            ),
                         ),
                         least_speaker_text,
                     ]
@@ -77,9 +94,13 @@ def main():
                 if recording_states[index]:
                     record_buttons[index].icon = ft.icons.MIC
                     record_buttons[index].icon_color = ft.colors.GREEN
+                    # diarization_pipeline.clear_file()
                 else:
                     record_buttons[index].icon = ft.icons.MIC_OFF
                     record_buttons[index].icon_color = ft.colors.RED
+                    global diarization_pipeline
+                    diarization_pipeline.register_id(name_fields[index].value)
+
                 page.update()
 
             return handler
@@ -88,12 +109,22 @@ def main():
             if speaker_count.value == "-":
                 page.controls.clear()
                 page.add(
-                    create_centered_container([ft.Text("話者の人数を選択してください:"), speaker_count, start_button])
+                    create_centered_container(
+                        [
+                            ft.Text("話者の人数を選択してください:"),
+                            speaker_count,
+                            start_button,
+                        ]
+                    )
                 )
             else:
                 num_speakers = int(speaker_count.value)
                 name_and_record_fields = create_name_and_record_fields(
-                    num_speakers, name_fields, record_buttons, recording_states, toggle_recording
+                    num_speakers,
+                    name_fields,
+                    record_buttons,
+                    recording_states,
+                    toggle_recording,
                 )
                 page.controls.clear()
                 page.add(
@@ -103,11 +134,17 @@ def main():
                         + [start_button]
                     )
                 )
+            global diarization_pipeline
+            diarization_pipeline = MySpeakerDiarization(speaker_num=num_speakers)
             page.update()
 
         start_button = ft.ElevatedButton(text="開始", on_click=start_recording)
         speaker_count.on_change = on_speaker_count_change
 
-        page.add(create_centered_container([ft.Text("話者の人数を選択してください:"), speaker_count, start_button]))
+        page.add(
+            create_centered_container(
+                [ft.Text("話者の人数を選択してください:"), speaker_count, start_button]
+            )
+        )
 
     ft.app(target=main_app)
--- interface/speaker_diarization/diarization.py
+++ interface/speaker_diarization/diarization.py
@@ -1,10 +1,91 @@
-# import diart
+from collections import defaultdict
+from diart import SpeakerDiarization, SpeakerDiarizationConfig
+from diart.sources import MicrophoneAudioSource
+from diart.inference import StreamingInference
+from diart.sinks import RTTMWriter
+import os
+import requests
+import json
 
 
-class SpeakerDiarization:
-    def __init__(self):
-        # 初期設定などをここに記述
-        pass
+class MySpeakerDiarization:
+    def __init__(self, id_name=defaultdict(str), speaker_num=5):
+        self.id_name = id_name
+        self.speaker_num = speaker_num
+        config = SpeakerDiarizationConfig(
+            max_speakers=speaker_num,
+            step=0.5,
+            latency=0.5,
+            tau_active=0.555,
+            rho_update=0.422,
+            delta_new=1.517,
+        )
+        pipeline = SpeakerDiarization(config)
+        mic = MicrophoneAudioSource()
+        inference = StreamingInference(pipeline, mic)
+        inference.attach_observers(RTTMWriter(mic.uri, "testtesttest"))
 
     def start(self):
-        pass
+        self.clear_file()
+
+    def associate_id2name(self, speaker_id, name):
+        self.id_name[speaker_id] = name
+
+    def register_id(self, name):
+        file_path = "file.rttm"
+        with open(file_path, "r", encoding="utf-8") as f:
+            lines = f.readlines()
+
+        durations = defaultdict(float)
+        for line in lines:
+            if line.startswith("SPEAKER"):
+                data = line.split(" ")
+                speaker_id = data[7]
+                durations[speaker_id] = float(data[4]) + durations[speaker_id]
+
+        durations = list(durations.items())
+        max_id = max(durations, key=lambda x: x[1])[0]
+        print(self.id_name)
+        self.associate_id2name(max_id, name)
+        print(self.id_name)
+
+    def clear_file(self):
+        file_path = "file.rttm"
+        with open(file_path, "w", encoding="utf-8") as f:
+            f.write("")
+
+    def save_to_server(self):
+        url = "http://localhost:5000/register"
+        headers = {"Content-Type": "application/json"}
+        data = {"n": len(self.speaker_num)}
+        res = requests.post(url, data=json.dumps(data), headers=headers)
+
+        url = "http://localhost:5000/send"
+        file_path = "file.rttm"
+        with open(file_path, "r", encoding="utf-8") as f:
+            lines = f.readlines()
+
+        id_and_durations = defaultdict(list)
+        for line in lines:
+            if line.startswith("SPEAKER"):
+                data = line.split(" ")
+                # data[7]はspeaker0, speaker1, ...のような形式。ここから数字を取り出す
+                speaker_id = data[7].split("r")[1]
+                id_and_durations[speaker_id].append(float(data[4]))
+
+        for i in range(len(self.speaker_num)):
+            data = {
+                "id": str(i),
+                "durations": str(id_and_durations[str(i)]),
+            }
+            res = requests.post(url, data=json.dumps(data), headers=headers)
+            print(res.text)
+
+    def fetch_from_server(self):
+        url = "http://localhost:5000/get_speaking_time"
+        headers = {"Content-Type": "application/json"}
+        data = defaultdict(float)
+        for i in range(len(self.speaker_num)):
+            res = requests.get(url, headers=headers, json={"id": i})
+            data[i] = float(res.text)  # TODO
+        return data
